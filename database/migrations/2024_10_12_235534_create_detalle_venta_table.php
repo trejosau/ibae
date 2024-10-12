795d699e7d9891995b2f3d65a418e0fedd8be1fd
trejosau <albertosaut@gmail.com>
2024-10-12 17:55:58 -0600

    agregando migracion detalle_venta y info

---
+++ database/migrations/2024_10_12_235534_create_detalle_venta_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('detalle_venta', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_venta');
+            $table->unsignedBigInteger('id_producto');
+            $table->decimal('precio_aplicado', 10, 2);
+            $table->decimal('descuento', 10, 2)->default(0);
+            $table->foreign('id_venta')->references('id')->on('ventas')->onDelete('cascade');
+            $table->foreign('id_producto')->references('id')->on('productos')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_venta');
+    }
+};
