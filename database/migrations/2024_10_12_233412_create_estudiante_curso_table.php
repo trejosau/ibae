fb7e40db8bcf71a97e4e2cbd18a37539f63245c5
trejosau <albertosaut@gmail.com>
2024-10-12 17:44:56 -0600

    agregando migracion estudiante_curso y su info

--- database/migrations/2024_10_12_233412_create_estudiante_curso_table.php
+++ database/migrations/2024_10_12_233412_create_estudiante_curso_table.php
@@ -4,25 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEstudianteCursoTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('estudiante_curso', function (Blueprint $table) {
-            $table->id(); // Crea un campo "id" auto-incremental
+            $table->id();
             $table->unsignedBigInteger('id_estudiante');
             $table->unsignedBigInteger('id_curso_apertura');
             $table->date('fecha_inscripcion');
             $table->integer('asistencia')->default(0);
-
-            // Define las claves foráneas
-            $table->foreign('id_estudiante')->references('matricula')->on('estudiantes')->onDelete('cascade');
-            $table->foreign('id_curso_apertura')->references('id')->on('curso_apertura')->onDelete('cascade');
+            $table->foreign('id_estudiante')->references('matricula')->on('estudiantes');
+            $table->foreign('id_curso_apertura')->references('id')->on('curso_apertura');
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('estudiante_curso');
     }
-}
+};
