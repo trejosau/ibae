d8402bf68823cf292bf57e09bbcad7f3d3098f40
trejosau <albertosaut@gmail.com>
2024-10-12 17:57:19 -0600

    agregando migracion compras y info

---
+++ database/migrations/2024_10_12_235702_create_compras_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_proveedor');
+            $table->date('fecha_compra');
+            $table->date('fecha_entrega');
+            $table->enum('estado', ['entregado', 'pendiente', 'cancelado']);
+            $table->text('motivo')->nullable();
+            $table->decimal('total', 10, 2);
+            $table->foreign('id_proveedor')->references('id')->on('proveedores')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('compras');
+    }
+};
