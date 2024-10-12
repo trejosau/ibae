7b4ccd9dbcc3edaa5e3270a66b47a9dec1f425de
trejosau <albertosaut@gmail.com>
2024-10-12 17:14:31 -0600

    quite notnullable ya que no hace falta este metodo

--- database/migrations/2024_10_12_230957_create_certificados_table.php
+++ database/migrations/2024_10_12_230957_create_certificados_table.php
@@ -1,23 +1,25 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-public function up(): void
-{
-Schema::create('certificados', function (Blueprint $table) {
-$table->id(); // ID del certificado
-$table->string('nombre', 255)->notNullable(); // Nombre del certificado
-$table->text('descripcion')->nullable(); // Descripción del certificado
-$table->integer('horas')->notNullable(); // Número de horas del certificado
-$table->enum('institucion', ['SEP', 'Otra'])->notNullable(); // Institución que otorga el certificado
-$table->timestamps(); // Timestamps por defecto
-});
-}
+    public function up(): void
+    {
+        Schema::create('certificados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 255);
+            $table->text('descripcion')->nullable();
+            $table->integer('horas');
+            $table->enum('institucion', ['SEP', 'Otra']);
+            $table->timestamps();
+        });
+    }
 
-public function down(): void
-{
-Schema::dropIfExists('certificados');
-}
+    public function down(): void
+    {
+        Schema::dropIfExists('certificados');
+    }
 };
