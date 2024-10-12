cc98d9e7845d1eb8b3936528a6a2cfc389646f20
trejosau <albertosaut@gmail.com>
2024-10-12 17:50:05 -0600

    agregando migracion productos y su info

---
+++ database/migrations/2024_10_12_234931_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->string('marca', 100);
+            $table->decimal('precio_lista', 10, 2);
+            $table->decimal('precio_venta', 10, 2);
+            $table->integer('cantidad');
+            $table->enum('medida', ['pzas', 'ml', 'lt', 'gr', 'cm']);
+            $table->unsignedBigInteger('id_proveedor');
+            $table->string('main_photo')->nullable();
+            $table->integer('stock');
+            $table->enum('estado', ['activo', 'inactivo', 'agotado']);
+            $table->date('fecha_agregado');
+            $table->unsignedBigInteger('id_categoria');
+            $table->foreign('id_proveedor')->references('id')->on('proveedores')->onDelete('cascade');
+            $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
