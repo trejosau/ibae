73f861d842b633cad1d99689d28b544985477cd7
trejosau <albertosaut@gmail.com>
2024-10-12 17:10:47 -0600

    agrego migracion tabla estilistas

--- database/migrations/2024_10_12_214347_create_estilistas_table.php
+++ database/migrations/2024_10_12_214347_create_estilistas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('estilistas', function (Blueprint $table) {
             $table->id();
+            $table->enum('estado', ['activo', 'inactivo', 'vacaciones']);
+            $table->unsignedBigInteger('id_persona');
+            $table->foreign('id_persona')->references('id')->on('personas')->onDelete('cascade');
             $table->timestamps();
         });
     }
