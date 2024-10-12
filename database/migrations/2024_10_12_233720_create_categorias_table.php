4472f203cff8c689488684b2793d8a0540de3cd8
trejosau <albertosaut@gmail.com>
2024-10-12 17:44:28 -0600

    agregando migracion modulo y su info

--- database/migrations/2024_10_12_233720_create_categorias_table.php
+++ database/migrations/2024_10_12_233720_create_categorias_table.php
@@ -6,17 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('modulos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->integer('duracion');
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('modulos');
+        Schema::dropIfExists('categorias');
     }
 };
