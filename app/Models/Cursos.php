cdc169542ef41ff84c5d873794957847fb820850
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:15 -0600

    Agregar contenido al modelo Cursos

--- app/Models/Cursos.php
+++ app/Models/Cursos.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cursos extends Model
 {
     use HasFactory;
+
+    protected $table = 'cursos';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'duracion_semanas',
+        'id_certificacion',
+    ];
+
+    public function certificado() : BelongsTo
+    {
+        return $this->belongsTo(Certificados::class, 'id_certificacion');
+    }
+
+    public function cursoApertura() : HasMany
+    {
+        return $this->hasMany(CursoApertura::class, 'id_curso');
+    }
 }
