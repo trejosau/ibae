336b6dfe876b69455d7655d8f0511d5ed69f5563
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:13 -0600

    Agregar contenido al modelo Colegiaturas

--- app/Models/Colegiaturas.php
+++ app/Models/Colegiaturas.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Colegiaturas extends Model
 {
     use HasFactory;
+
+    protected $table = 'colegiaturas';
+
+    protected $fillable = [
+        'id_estudiante_curso',
+        'semana',
+        'Asistio',
+        'Fecha_de_Pago',
+        'estado',
+        'Monto',
+    ];
+
+    public function estudianteCurso() : BelongsTo
+    {
+        return $this->belongsTo(EstudianteCurso::class, 'id_estudiante_curso');
+    }
 }
