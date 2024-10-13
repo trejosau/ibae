e8a61ecfd6dd7b4143a1363d86e05c2b18980a97
trejosau <albertosaut@gmail.com>
2024-10-12 18:04:42 -0600

    Agregando migracion detalle_citas

---
+++ database/migrations/2024_10_13_000433_create_detalle_cita_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('detalle_cita', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cita');
+            $table->unsignedBigInteger('id_servicio');
+            $table->foreign('id_cita')->references('id')->on('citas')->onDelete('cascade');
+            $table->foreign('id_servicio')->references('id')->on('servicios')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('detalle_cita');
+    }
+};
