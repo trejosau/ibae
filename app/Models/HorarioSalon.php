a9a1536c4da0652288febff5f5ac4c28d23e2566
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:21 -0600

    Agregar contenido al modelo HorarioSalon

--- app/Models/HorarioSalon.php
+++ app/Models/HorarioSalon.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class HorarioSalon extends Model
 {
     use HasFactory;
+
+    protected $table = 'horario_salon';
+
+    protected $fillable = [
+        'dia',
+        'hora_apertura',
+        'hora_cierre',
+    ];
+
 }
