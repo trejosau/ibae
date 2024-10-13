a702971d8e975d2e32c0549b86e772d30d911cc1
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:14 -0600

    Agregar contenido al modelo Comprador

--- app/Models/Comprador.php
+++ app/Models/Comprador.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comprador extends Model
 {
     use HasFactory;
+
+    protected $table = 'compradores';
+
+    protected $fillable = [
+        'id_persona',
+        'preferencia',
+        'razon_social',
+    ];
+
+    public function persona() : BelongsTo
+    {
+        return $this->belongsTo(Persona::class, 'id_persona');
+    }
 }
