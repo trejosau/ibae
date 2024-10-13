2cfffb308aaaa455bf6cb8109fec66c7a1c196bb
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:18 -0600

    Agregar contenido al modelo DetalleVenta

--- app/Models/DetalleVenta.php
+++ app/Models/DetalleVenta.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetalleVenta extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_venta';
+
+    protected $fillable = [
+        'id_venta',
+        'id_producto',
+        'precio_aplicado',
+        'descuento',
+    ];
+
+
+    public function venta() : BelongsTo
+    {
+        return $this->belongsTo(Ventas::class, 'id_venta');
+    }
+
+
+    public function producto() : BelongsTo
+    {
+        return $this->belongsTo(Productos::class, 'id_producto');
+    }
 }
