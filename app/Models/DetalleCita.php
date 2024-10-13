deb13a8061801708d6a4e0fb52f8d2871d7c7e6e
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:16 -0600

    Agregar contenido al modelo DetalleCita

--- app/Models/DetalleCita.php
+++ app/Models/DetalleCita.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetalleCita extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_cita';
+
+    protected $fillable = [
+        'id_cita',
+        'id_servicio',
+    ];
+
+    public function cita() : BelongsTo
+    {
+        return $this->belongsTo(Citas::class, 'id_cita');
+    }
+
+    public function servicio() : BelongsTo
+    {
+        return $this->belongsTo(Servicios::class, 'id_servicio');
+    }
 }
