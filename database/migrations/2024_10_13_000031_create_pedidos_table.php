fd07aaab7655a12186a3a720520f36ff4c32f80b
trejosau <albertosaut@gmail.com>
2024-10-12 18:00:57 -0600

    Agregando migraciónpedidos

---
+++ database/migrations/2024_10_13_000031_create_pedidos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total', 10, 2);
+            $table->date('fecha_pedido');
+            $table->enum('estado', ['entregado', 'listo para entrega', 'preparando para entrega']);
+            $table->string('clave_entrega', 100);
+            $table->unsignedBigInteger('id_comprador');
+            $table->enum('es_estudiante', ['si', 'no']);
+            $table->foreign('id_comprador')->references('id')->on('compradores')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
