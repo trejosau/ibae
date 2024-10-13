004fd69580b5dadf59552b495fc92f8fb3c86dee
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:13 -0600

    Agregar contenido al modelo Citas

--- app/Models/Citas.php
+++ app/Models/Citas.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Citas extends Model
 {
     use HasFactory;
+
+
+    protected $table = 'citas';
+
+
+    protected $fillable = [
+        'id_estilista',
+        'id_comprador',
+        'fecha_hora_creacion',
+        'fecha_hora_inicio_cita',
+        'fecha_hora_fin_cita',
+    ];
+
+    public function estilista() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Estilista::class, 'id_estilista');
+    }
+
+    public function comprador() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Comprador::class, 'id_comprador');
+    }
 }
