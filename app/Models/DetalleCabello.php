6e83239df4016656ddec8b7873f63b88ba73eced
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:16 -0600

    Agregar contenido al modelo DetalleCabello

--- app/Models/DetalleCabello.php
+++ app/Models/DetalleCabello.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetalleCabello extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_cabello';
+
+    protected $fillable = [
+        'id_detalle_cita',
+        'largo',
+        'volumen',
+        'estado',
+    ];
+
+    public function detalleCita() : BelongsTo
+    {
+        return $this->belongsTo(DetalleCita::class, 'id_detalle_cita');
+    }
 }
