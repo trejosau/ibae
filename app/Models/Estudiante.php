7240b2e74e2ab4b9cc3fa7373b6490f173e4fc38
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:19 -0600

    Agregar contenido al modelo Estudiante

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 
 class Estudiante extends Model
 {
     use HasFactory;
+
+
+    protected $table = 'estudiantes';
+
+
+    protected $fillable = [
+        'id_persona',
+        'id_inscripcion',
+        'fecha_inscripcion',
+        'grado_estudio',
+        'zipcode',
+        'colonia',
+        'calle',
+        'num_ext',
+        'num_int',
+    ];
+
+    public function persona() : BelongsTo
+    {
+        return $this->belongsTo(Persona::class, 'id_persona');
+    }
+
+    public function inscripcion() : BelongsTo
+    {
+        return $this->belongsTo(Inscripcion::class, 'id_inscripcion');
+    }
 }
