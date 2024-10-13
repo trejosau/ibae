0b7b102c605642a507cadaf1c5f2c2a31991cbc2
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:11 -0600

    Agregar contenido al modelo Administrador

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Administrador extends Model
 {
     use HasFactory;
+
+    protected $table = 'administradores';
+
+    protected $fillable = [
+        'id_persona',
+    ];
+
+    public function persona() : BelongsTo
+    {
+        return $this->belongsTo(Persona::class, 'id_persona');
+    }
 }
