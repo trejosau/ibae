0905b9d68eafb62dc5577291c45e4d849f0274f9
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:22 -0600

    Agregar contenido al modelo ModuloCurso

--- app/Models/ModuloCurso.php
+++ app/Models/ModuloCurso.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ModuloCurso extends Model
 {
     use HasFactory;
+
+    protected $table = 'modulo_curso';
+
+    protected $fillable = [
+        'id_modulo',
+        'id_curso',
+        'orden',
+        'id_profesor',
+    ];
+
+    public function modulo(): BelongsTo
+    {
+        return $this->belongsTo(Modulos::class, 'id_modulo');
+    }
+
+    public function curso(): BelongsTo
+    {
+        return $this->belongsTo(Cursos::class, 'id_curso');
+    }
+
+    public function profesor(): BelongsTo
+    {
+        return $this->belongsTo(Profesor::class, 'id_profesor');
+    }
 }
