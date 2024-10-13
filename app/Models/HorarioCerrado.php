2fb9104620ff163d8dc08b95fcb7cf86974e6ebc
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:20 -0600

    Agregar contenido al modelo HorarioCerrado

--- app/Models/HorarioCerrado.php
+++ app/Models/HorarioCerrado.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class HorarioCerrado extends Model
 {
     use HasFactory;
+
+    protected $table = 'horario_cerrado'; // Especifica el nombre de la tabla si no sigue la convención de plural
+
+    protected $fillable = [
+        'fecha_hora_cierre_inicio',
+        'fecha_hora_cierre_fin',
+        'hora_fin',
+        'motivo',
+    ];
+
+
 }
