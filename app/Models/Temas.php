fc9d4171e9b3e82696c9155a822a1e28f87d9809
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:25 -0600

    Agregar contenido al modelo Temas

--- app/Models/Temas.php
+++ app/Models/Temas.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Temas extends Model
 {
-    use HasFactory;
+    protected $table = 'temas';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+    ];
+
+    public function moduloTemas() : HasMany
+    {
+        return $this->hasMany(ModuloTemas::class, 'id_tema');
+    }
 }
+
