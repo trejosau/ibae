92eb96ac12c646df53510813374ac7bb38375f81
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:25 -0600

    Agregar contenido al modelo Servicios

--- app/Models/Servicios.php
+++ app/Models/Servicios.php
@@ -1,11 +1,22 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Servicios extends Model
 {
-    use HasFactory;
+    protected $table = 'servicios';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+        'categoria',
+    ];
+
+    public function detallesCita() : HasMany
+    {
+        return $this->hasMany(DetalleCita::class, 'id_servicio');
+    }
 }
