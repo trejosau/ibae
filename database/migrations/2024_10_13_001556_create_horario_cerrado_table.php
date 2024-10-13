a2d68ded957c897da6278cbe0f72f4d83d26f15b
trejosau <albertosaut@gmail.com>
2024-10-12 18:16:29 -0600

    agregando migracion de la tabla horario_cerrado

---
+++ database/migrations/2024_10_13_001556_create_horario_cerrado_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHorarioCerradoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horario_cerrado', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('fecha_hora_cierre_inicio');
+            $table->dateTime('fecha_hora_cierre_fin');
+            $table->time('hora_fin');
+            $table->string('motivo', 255);
+            $table->timestamps(); // Esto agrega created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horario_cerrado');
+    }
+}
