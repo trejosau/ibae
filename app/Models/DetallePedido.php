126ae74f14df481b39b4f0292f3c4dd44d99d7f6
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:17 -0600

    Agregar contenido al modelo DetallePedido

--- app/Models/DetallePedido.php
+++ app/Models/DetallePedido.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetallePedido extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_pedido';
+
+    protected $fillable = [
+        'id_pedido',
+        'id_producto',
+        'cantidad',
+        'precio_aplicado',
+        'descuento',
+    ];
+
+    public function pedido() : BelongsTo
+    {
+        return $this->belongsTo(Pedidos::class, 'id_pedido');
+    }
+
+    public function producto() : BelongsTo
+    {
+        return $this->belongsTo(Productos::class, 'id_producto');
+    }
 }
