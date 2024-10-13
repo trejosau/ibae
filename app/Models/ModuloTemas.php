23a5e002347bd664be49ad21aafd01e9d0bb4a65
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:22 -0600

    Agregar contenido al modelo ModuloTemas

--- app/Models/ModuloTemas.php
+++ app/Models/ModuloTemas.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ModuloTemas extends Model
 {
-    use HasFactory;
+    protected $table = 'modulo_temas';
+
+    protected $fillable = [
+        'id_modulo',
+        'id_tema',
+    ];
+
+    public function modulo(): BelongsTo
+    {
+        return $this->belongsTo(Modulos::class, 'id_modulo');
+    }
+
+    public function tema(): BelongsTo
+    {
+        return $this->belongsTo(Temas::class, 'id_tema');
+    }
 }
