4bfce757b8ee8bc27aca622f8f523842cb2b9ce9
trejosau <albertosaut@gmail.com>
2024-10-12 18:01:44 -0600

    Agregando migrracion detalle_pedido

---
+++ database/migrations/2024_10_13_000129_create_detalle_pedido_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('detalle_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_pedido');
+            $table->unsignedBigInteger('id_producto');
+            $table->integer('cantidad');
+            $table->decimal('precio_aplicado', 10, 2);
+            $table->decimal('descuento', 10, 2)->default(0);
+            $table->foreign('id_pedido')->references('id')->on('pedidos')->onDelete('cascade');
+            $table->foreign('id_producto')->references('id')->on('productos')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('detalle_pedido');
+    }
+};
