f897629ea4c67c3adbf22382cfdb838f707844ff
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:14 -0600

    Agregar contenido al modelo Compras

--- app/Models/Compras.php
+++ app/Models/Compras.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Compras extends Model
 {
     use HasFactory;
+
+    protected $table = 'compras';
+
+    protected $fillable = [
+        'id_proveedor',
+        'fecha_compra',
+        'fecha_entrega',
+        'estado',
+        'motivo',
+        'total',
+    ];
+
+    public function proveedor() : BelongsTo
+    {
+        return $this->belongsTo(Proveedores::class, 'id_proveedor');
+    }
 }
