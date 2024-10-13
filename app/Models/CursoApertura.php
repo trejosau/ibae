bc664c0bbea88cce1d04e7c13f0fc59402502c72
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:15 -0600

    Agregar contenido al modelo CursoApertura

--- app/Models/CursoApertura.php
+++ app/Models/CursoApertura.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CursoApertura extends Model
 {
     use HasFactory;
+
+    protected $table = 'curso_apertura';
+
+    protected $fillable = [
+        'id_curso',
+        'nombre',
+        'fecha_inicio',
+        'periodo',
+        'año',
+    ];
+
+    public function curso() : BelongsTo
+    {
+        return $this->belongsTo(Cursos::class, 'id_curso');
+    }
 }
