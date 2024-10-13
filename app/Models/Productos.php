ad1462d325e43ccace67d9eb7299f6ca113f41ba
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:24 -0600

    Agregar contenido al modelo Productos

--- app/Models/Productos.php
+++ app/Models/Productos.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Productos extends Model
 {
-    use HasFactory;
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'marca',
+        'precio_lista',
+        'precio_venta',
+        'cantidad',
+        'medida',
+        'id_proveedor',
+        'main_photo',
+        'stock',
+        'estado',
+        'fecha_agregado',
+        'id_categoria'
+    ];
+
+    public function proveedor(): BelongsTo
+    {
+        return $this->belongsTo(Proveedores::class, 'id_proveedor');
+    }
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categorias::class, 'id_categoria');
+    }
 }
