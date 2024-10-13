ce2b6cfd4eaa4e9aca560f30ee53b1d03cec16a2
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:27 -0600

    Agregar contenido al modelo Ventas

--- app/Models/Ventas.php
+++ app/Models/Ventas.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ventas extends Model
 {
-    use HasFactory;
+    protected $table = 'ventas';
+
+    protected $fillable = [
+        'id_comprador',
+        'fecha_compra',
+        'total',
+        'id_admin',
+        'es_estudiante',
+    ];
 }
