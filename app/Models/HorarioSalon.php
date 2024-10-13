11ac3cd3ff5b5dabf8cd1401e4129aef77383c75
trejosau <albertosaut@gmail.com>
2024-10-12 18:29:08 -0600

    agregando todos los modelos

---
+++ app/Models/HorarioSalon.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HorarioSalon extends Model
+{
+    use HasFactory;
+}
