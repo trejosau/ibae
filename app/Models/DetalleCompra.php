ca7cd4a54f4ac882d6ffc329e4db51da7806f488
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:17 -0600

    Agregar contenido al modelo DetalleCompra

--- app/Models/DetalleCompra.php
+++ app/Models/DetalleCompra.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetalleCompra extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_compra';
+
+    protected $fillable = [
+        'id_compra',
+        'id_producto',
+        'cantidad',
+    ];
+
+    public function compra() : BelongsTo
+    {
+        return $this->belongsTo(Compras::class, 'id_compra');
+    }
+
+    public function producto() : BelongsTo
+    {
+        return $this->belongsTo(Productos::class, 'id_producto');
+    }
 }
