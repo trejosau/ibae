a1c9fc3599a0e8fe9f101c16301e1829a0f5ec5d
trejosau <albertosaut@gmail.com>
2024-10-12 19:44:19 -0600

    Agregar contenido al modelo Estilista

--- app/Models/Estilista.php
+++ app/Models/Estilista.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Estilista extends Model
 {
     use HasFactory;
+
+    protected $table = 'estilistas';
+
+    protected $fillable = [
+        'estado',
+        'id_persona',
+    ];
+
+    public function persona() : BelongsTo
+    {
+        return $this->belongsTo(Persona::class, 'id_persona');
+    }
+
+    public function citas() : HasMany
+    {
+        return $this->hasMany(Citas::class, 'id_estilista');
+    }
 }
