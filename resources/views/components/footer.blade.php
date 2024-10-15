d9370d7665a57db9ef73f3a1c7ea9c7fe41d5187
trejosau <albertosaut@gmail.com>
2024-10-14 22:09:27 -0600

    Footer del mismo color que body y ajustes en footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,29 +1,34 @@
-<footer class="footer bg-dark py-4">
+<footer class="footer py-5">
     <div class="container">
         <div class="row">
-            <!-- Sección de Atención al Cliente -->
-            <div class="col-md-6 mb-3">
-                <h5>Atención al Cliente</h5>
-                <ul>
-                    <li><a href="#">Contáctanos</a></li>
-                    <li><a href="#">Preguntas frecuentes</a></li>
-                </ul>
+            <div class="col-lg-8 mb-4 mx-auto text-center">
+                <a href="" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Cursos
+                </a>
+                <a href="" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Catalogo de productos
+                </a>
+                <a href="" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Servicios de belleza
+                </a>
             </div>
-
-            <!-- Sección Acerca de Nosotros -->
-            <div class="col-md-6">
-                <h5>Acerca de Nosotros</h5>
-                <ul>
-                    <li><a href="#">PEPE Historia</a></li>
-                    <li><a href="#">Cursos</a></li>
-                    <li><a href="#">Política de Privacidad</a></li>
-                    <li><a href="#">Catálogo de productos</a></li>
-                    <li><a href="#">Servicios de belleza</a></li>
-                </ul>
+            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-facebook"></span>
+                </a>
+                <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-instagram"></span>
+                </a>
             </div>
         </div>
-        <div class="text-center mt-3">
-            <small>&copy; 2024 IBA&E</small>
+        <div class="row">
+            <div class="col-8 mx-auto text-center mt-1">
+                <p class="mb-0 text-secondary">
+                    © <script>
+                        document.write(new Date().getFullYear())
+                    </script> Soft by IBA&E.
+                </p>
+            </div>
         </div>
     </div>
 </footer>
