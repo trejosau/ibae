b4fae99f2c9e72c0a9fa11e6361d50c52cae7963
trejosau <albertosaut@gmail.com>
2024-10-15 08:44:20 -0600

    cambia sidebar en dashboard al header y minheight al header

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,33 +1,22 @@
 @extends('layouts.app')
 
+
+
 @section('content')
     <div class="d-flex">
-        <!-- Sidebar -->
-        <nav class="sidebar bg-dark flex-shrink-0 p-3">
-            <h4 class="text-center">Menú</h4>
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Perfil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Configuracion</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Ayuda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cerrar sesión</a>
-                </li>
-            </ul>
-        </nav>
+
 
         <!-- Main Content -->
-        <div class="main-content flex-grow-1 p-3">
-             
-            hola
+        <div class="main-content flex-grow-1 p-3 ps-5">
+
+            @if (auth()->user()->roles->isNotEmpty())
+                <ul>
+                    @foreach (auth()->user()->roles as $role)
+                        Hola, {{ auth()->user()->username }}
+                        <li>{{ $role->name }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
         </div>
     </div>
