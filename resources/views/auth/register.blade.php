ece9eebb0f6dbce95cf3162a92069c1baf93cf92
trejosau <albertosaut@gmail.com>
2024-10-15 20:45:53 -0600

    login y registro mejorados a nivel diseño

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,56 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="card" style="width: 25rem;">
-            <div class="card-body">
-                <h5 class="card-title text-center">Registro</h5>
+    <section class="py-3 py-md-5 py-xl-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mb-5">
+                        <h2 class="display-5 fw-bold text-center">Registro</h2>
+                        <p class="text-center m-0">¿Ya tienes una cuenta? <a href="{{ route('login') }}">Inicia sesión aquí</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-10 col-xl-8">
+                    <div class="row gy-5 justify-content-center">
+                        <div class="col-12 col-lg-5">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <!-- Mensaje de éxito -->
+                                @if(session('success'))
+                                    <div class="alert alert-success">
+                                        {{ session('success') }}
+                                    </div>
+                                @endif
 
-                <!-- Mensaje de éxito -->
-                @if(session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                @endif
+                                <!-- Mensaje de error -->
+                                @if ($errors->any())
+                                    <div class="alert alert-danger pb-0">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
 
-                <!-- Mensaje de error -->
-                @if ($errors->any())
-                    <div class="alert alert-danger pb-0">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <ul>
-                                    {{ $error }}
-
-                                </ul>
-                            @endforeach
-                        </ul>
+                                <div class="row gy-3 overflow-hidden">
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control border-0 border-bottom rounded-0" name="username" id="username" required>
+                                            <label for="username" class="form-label">Username</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="email" class="form-control border-0 border-bottom rounded-0" name="email" id="email" required>
+                                            <label for="email" class="form-label">Email</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="password" class="form-control border-0 border-bottom rounded-0" name="password" id="password" required>
+                                            <label for="password" class="form-label">Password</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="password" class="form-control border-0 border-bottom rounded-0" name="password_confirmation" id="password_confirmation" required>
+                                            <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-primary btn-lg">Registrar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-12 col-lg-2 d-flex align-items-center justify-content-center gap-3 flex-lg-column">
+                            <div class="bg-dark h-100 d-none d-lg-block" style="width: 1px; --bs-bg-opacity: .1;"></div>
+                            <div class="bg-dark w-100 d-lg-none" style="height: 1px; --bs-bg-opacity: .1;"></div>
+                            <div>or</div>
+                            <div class="bg-dark h-100 d-none d-lg-block" style="width: 1px; --bs-bg-opacity: .1;"></div>
+                            <div class="bg-dark w-100 d-lg-none" style="height: 1px; --bs-bg-opacity: .1;"></div>
+                        </div>
+                        <div class="col-12 col-lg-5 d-flex align-items-center">
+                            <div class="d-flex gap-3 flex-column w-100">
+                                <a href="#" class="btn btn-lg btn-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+                                        <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                                    </svg>
+                                    <span class="ms-2 fs-6">Sign in with Google</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                @endif
-
-                <form method="POST" action="{{ route('register') }}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password_confirmation">Confirm Password</label>
-                        <input type="password" class="form-control" name="password_confirmation" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block mt-3">Registrar</button>
-                </form>
-                <div class="text-center mt-3">
-                    <a href="{{ route('login') }}">¿Ya tienes una cuenta? Inicia sesión aquí</a>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
