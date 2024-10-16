da34dbb883845c70941275fde7089f921885e114
LGSUSDLR <jesusmanueldelarosaguillen@gmail.com>
2024-10-16 08:36:11 -0600

    cambios en salon y agg imagen

--- resources/views/salon.blade.php
+++ resources/views/salon.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <main class="container">
-        <h1 class="dashboard-header text-center">Salon</h1>
+        <h1 class="dashboard-header text-center">Salon de Belleza</h1>
+        <img class="img-fluid" src="{{asset('images/brunette-woman-with-mobile-phone-getting-her-hair-done.jpg')}}" alt="">
     </main>
 @endsection
