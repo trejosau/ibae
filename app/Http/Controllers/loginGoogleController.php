7419c6943ac2c7ec5ab0fa642775750839ca8b30
trejosau <albertosaut@gmail.com>
2024-10-27 21:55:39 -0600

    arreglando login con google, cuando te regstraba te enviaba a login siempre, ahora te manda a home aunque no este registrado

--- app/Http/Controllers/loginGoogleController.php
+++ app/Http/Controllers/loginGoogleController.php
@@ -8,7 +8,7 @@ use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
-class loginGoogleController extends Controller
+class LoginGoogleController extends Controller
 {
     public function redirectToGoogle()
     {
@@ -20,31 +20,23 @@ class loginGoogleController extends Controller
         try {
             $googleUser = Socialite::driver('google')->stateless()->user();
 
-            // Verificar si el usuario ya existe en la base de datos
             $user = User::where('email', $googleUser->email)->first();
 
-            // Si no existe, lo creamos y asignamos el rol "Comprador"
             if (!$user) {
-                // Extraer la parte del correo antes del '@' para el username
                 $username = explode('@', $googleUser->email)[0];
-
-                // Asegurarse de que el username sea único en la base de datos
                 while (User::where('username', $username)->exists()) {
-                    // Si el username ya existe, le añadimos un número aleatorio para hacerlo único
                     $username = substr($username, 0, 9) . rand(100, 999);
                 }
 
-                // Crear el usuario
                 $user = User::create([
-                    'username' => $username, // Usar la parte del correo antes del '@'
+                    'username' => $username,
                     'email' => $googleUser->email,
-                    'password' => Hash::make(uniqid()), // Generar una contraseña aleatoria
+                    'password' => Hash::make(uniqid()),
                 ]);
 
-                // Asignar el rol de "Comprador" al nuevo usuario
                 $user->assignRole('Comprador');
             }
-            // Autenticar al usuario
+
             Auth::login($user);
 
             return redirect()->route('home');
