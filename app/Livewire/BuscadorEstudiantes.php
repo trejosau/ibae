22afd6df3dc8fe0e66b420d1e209daacd644c65d
Ubuntu <ubuntu@institutoibae.com>
2024-11-19 17:21:04 +0000

    iniciando componente livewire para buscar estudiantes

---
+++ app/Livewire/BuscadorEstudiantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BuscadorEstudiantes extends Component
+{
+    public function render()
+    {
+        return view('livewire.buscador-estudiantes');
+    }
+}
